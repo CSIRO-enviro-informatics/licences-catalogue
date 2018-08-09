84572b18110ab0594433d86bca71690d86631526
Guillory, Laura (L&W, Dutton Park) <Laura.Guillory@csiro.au>
2018-08-09 12:33:41 +1000

    Completed db_access.py and unit tests

--- controller/db_access.py
+++ controller/db_access.py
@@ -5,6 +5,7 @@ import re
 # DB
 conn = sqlite3.connect(conf.DATABASE_PATH)
 conn.execute('PRAGMA foreign_keys = 1')
+conn.row_factory = sqlite3.Row  # Allows for accessing query results like a dictionary which is more readable
 cursor = conn.cursor()
 
 
@@ -12,10 +13,9 @@ def create_policy(uri):
     uri = str(uri)
     if not is_valid_uri(uri):
         raise ValueError('Not a valid URI: ' + uri)
-    try:
-        cursor.execute('INSERT INTO POLICY (URI) VALUES ("{uri:s}");'.format(uri=uri))
-    except sqlite3.IntegrityError:
+    if policy_exists(uri):
         raise ValueError('A Policy with that URI already exists.')
+    cursor.execute('INSERT INTO POLICY (URI) VALUES ("{uri:s}");'.format(uri=uri))
     conn.commit()
     return cursor.lastrowid
 
@@ -24,6 +24,165 @@ def is_valid_uri(uri):
     return True if re.match('\w+:(/?/?)[^\s]+', uri) else False
 
 
-def set_policy_type(uri, policy_type):
-    cursor.execute('UPDATE POLICY SET TYPE = "{type:s}" WHERE URI = "{uri:s}";'.format(type=policy_type, uri=uri))
+def set_policy_attribute(uri, attr, value):
+    permitted_attributes = ['TYPE', 'LABEL', 'JURISDICTION', 'LEGAL_CODE', 'HAS_VERSION', 'LANGUAGE', 'SEE_ALSO',
+                            'SAME_AS', 'COMMENT', 'LOGO', 'STATUS']
+    if attr not in permitted_attributes:
+        raise ValueError('Attribute \'' + attr + '\' is not permitted.')
+    if not policy_exists(uri):
+        raise ValueError('Policy with URI ' + uri + ' does not exist.')
+    conn.execute(
+        'UPDATE POLICY SET {attr:s} = "{value:s}" WHERE URI = "{uri:s}";'.format(attr=attr, value=value, uri=uri)
+    )
+    conn.commit()
+
+
+def policy_exists(uri):
+    cursor.execute('SELECT COUNT(1) FROM POLICY WHERE URI = "{uri:s}"'.format(uri=uri))
+    return cursor.fetchone()[0]
+
+
+def delete_policy(uri):
+    conn.execute('DELETE FROM POLICY WHERE URI = "{uri:s}"'.format(uri=uri))
+    conn.commit()
+
+
+def get_policy(uri):
+    if not policy_exists(uri):
+        raise ValueError('Policy with URI ' + uri + 'does not exist.')
+    cursor.execute('SELECT * FROM POLICY WHERE URI = "{uri:s}"'.format(uri=uri))
+    return dict(cursor.fetchone())
+
+
+def add_asset(uri, policy_uri):
+    uri = str(uri)
+    if not is_valid_uri(uri):
+        raise ValueError('Not a valid URI: ' + uri)
+    if not policy_exists(policy_uri):
+        raise ValueError('Policy with URI ' + uri + 'does not exist.')
+    if asset_exists(uri):
+        raise ValueError('An Asset with that URI already exists.')
+    cursor.execute('SELECT ID FROM POLICY WHERE URI="{policy_uri:s}"'.format(policy_uri=policy_uri))
+    policy_id = cursor.fetchone()['ID']
+    cursor.execute(
+        'INSERT INTO ASSET (URI, POLICY_ID) VALUES ("{uri:s}", {policy_id})'.format(uri=uri, policy_id=policy_id)
+    )
+    conn.commit()
+    return cursor.lastrowid
+
+
+def asset_exists(uri):
+    cursor.execute('SELECT COUNT(1) FROM ASSET WHERE URI = "{uri:s}"'.format(uri=uri))
+    return cursor.fetchone()[0]
+
+
+def remove_asset(uri):
+    conn.execute('DELETE FROM ASSET WHERE URI = "{uri:s}"'.format(uri=uri))
+    conn.commit()
+
+
+def create_rule(rule_type):
+    try:
+        cursor.execute('INSERT INTO RULE (TYPE) VALUES ("{rule_type:s}")'.format(rule_type=rule_type))
+    except sqlite3.IntegrityError:
+        raise ValueError('Rule type ' + rule_type + ' is not permitted.')
+    conn.commit()
+    return cursor.lastrowid
+
+
+def create_party(uri):
+    uri = str(uri)
+    if not is_valid_uri(uri):
+        raise ValueError('Not a valid URI: ' + uri)
+    if party_exists(uri):
+        raise ValueError('A Party with that URI already exists.')
+    cursor.execute('INSERT INTO PARTY (URI) VALUES ("{uri:s}")'.format(uri=uri))
+    conn.commit()
+    return cursor.lastrowid
+
+
+def party_exists(uri):
+    cursor.execute('SELECT COUNT(1) FROM PARTY WHERE URI = "{uri:s}"'.format(uri=uri))
+    return cursor.fetchone()[0]
+
+
+def delete_party(uri):
+    conn.execute('DELETE FROM PARTY WHERE URI = "{uri:s}"'.format(uri=uri))
+    conn.commit()
+
+
+def rule_exists(rule_id):
+    cursor.execute('SELECT COUNT(1) FROM RULE WHERE ID = {id}'.format(id=rule_id))
+    return cursor.fetchone()[0]
+
+
+def add_action_to_rule(rule_id, action_uri):
+    if not rule_exists(rule_id):
+        raise ValueError('Rule with ID ' + str(rule_id) + ' does not exist.')
+    cursor.execute('SELECT ID FROM ACTION WHERE URI = "{uri:s}"'.format(uri=action_uri))
+    result = cursor.fetchone()
+    if result is None:
+        raise ValueError('Action with URI ' + action_uri + ' does not exist.')
+    action_id = result['ID']
+    query = 'INSERT INTO RULE_HAS_ACTION (RULE_ID, ACTION_ID) VALUES ({rule_id}, {action_id})'
+    conn.execute(query.format(rule_id=rule_id, action_id=action_id))
+    conn.commit()
+
+
+def remove_action_from_rule(rule_id, action_uri):
+    cursor.execute('SELECT ID FROM ACTION WHERE URI = "{uri:s}"'.format(uri=action_uri))
+    result = cursor.fetchone()
+    if result is None:
+        raise ValueError('Action with URI ' + action_uri + ' does not exist.')
+    action_id = result['ID']
+    query = 'DELETE FROM RULE_HAS_ACTION WHERE RULE_ID = {rule_id} AND ACTION_ID={action_id}'
+    conn.execute(query.format(rule_id=rule_id, action_id=action_id))
+    conn.commit()
+
+
+def get_party_id(uri):
+    cursor.execute('SELECT ID FROM PARTY WHERE URI = "{uri}"'.format(uri=uri))
+    result = cursor.fetchone()
+    if result is None:
+        raise ValueError('Party with URI ' + uri + ' does not exist.')
+    return result['ID']
+
+
+def add_assignor_to_rule(party_uri, rule_id):
+    if not rule_exists(rule_id):
+        raise ValueError('Rule with ID ' + str(rule_id) + ' does not exist.')
+    party_id = get_party_id(party_uri)
+    query = 'INSERT INTO RULE_HAS_ASSIGNOR (PARTY_ID, RULE_ID) VALUES ({party_id}, {rule_id})'
+    conn.execute(query.format(party_id=party_id, rule_id=rule_id))
+    conn.commit()
+
+
+def remove_assignor_from_rule(party_uri, rule_id):
+    query = '''
+        DELETE FROM RULE_HAS_ASSIGNOR 
+        WHERE RULE_ID= {rule_id} AND PARTY_ID IN (
+            SELECT ID FROM PARTY WHERE URI = "{party_uri}"
+        )
+    '''
+    conn.execute(query.format(party_uri=party_uri, rule_id=rule_id))
+    conn.commit()
+
+
+def add_assignee_to_rule(party_uri, rule_id):
+    if not rule_exists(rule_id):
+        raise ValueError('Rule with ID ' + str(rule_id) + ' does not exist.')
+    party_id = get_party_id(party_uri)
+    query = 'INSERT INTO RULE_HAS_ASSIGNEE (PARTY_ID, RULE_ID) VALUES ({party_id}, {rule_id})'
+    conn.execute(query.format(party_id=party_id, rule_id=rule_id))
+    conn.commit()
+
+
+def remove_assignee_from_rule(party_uri, rule_id):
+    query = '''
+        DELETE FROM RULE_HAS_ASSIGNEE
+        WHERE RULE_ID= {rule_id} AND PARTY_ID IN (
+            SELECT ID FROM PARTY WHERE URI = "{party_uri}"
+        )
+    '''
+    conn.execute(query.format(party_uri=party_uri, rule_id=rule_id))
     conn.commit()
--- tests/test_db_access.py
+++ tests/test_db_access.py
@@ -14,10 +14,11 @@ def setup_module():
     create_database.rebuild()
     db_access.conn = sqlite3.connect(_conf.DATABASE_PATH)
     db_access.conn.execute('PRAGMA foreign_keys = 1')
+    db_access.conn.row_factory = sqlite3.Row
     db_access.cursor = db_access.conn.cursor()
 
 
-def teardown_function():
+def setup_function():
     db_access.conn.execute('DELETE FROM RULE_HAS_ASSIGNEE;')
     db_access.conn.execute('DELETE FROM RULE_HAS_ASSIGNOR;')
     db_access.conn.execute('DELETE FROM PARTY;')
@@ -52,11 +53,321 @@ def test_create_policy():
         db_access.create_policy(new_policy_uri)
 
 
-def test_set_policy_type():
+def test_policy_exists():
+    # Should return true if the policy exists
+    uri = 'https://example.com#test'
+    db_access.create_policy(uri)
+    assert db_access.policy_exists(uri)
+
+    # Should return false if the policy doesn't exist
+    nonexistent_uri = 'https://example.com#nonexistent'
+    assert not db_access.policy_exists(nonexistent_uri)
+
+
+def test_set_policy_attribute():
+    # Should set a valid attribute for an existing policy
     uri = 'https://example.com#test'
     new_policy_type = 'http://creativecommons.org/ns#License'
     db_access.create_policy(uri)
-    db_access.set_policy_type(uri, new_policy_type)
+    db_access.set_policy_attribute(uri, 'TYPE', new_policy_type)
     db_access.cursor.execute('SELECT TYPE FROM POLICY WHERE URI = "{uri}";'.format(uri=uri))
     stored_policy_type = db_access.cursor.fetchone()[0]
     assert stored_policy_type == new_policy_type
+
+    # Should reject attribute change for a policy that does not exist
+    with pytest.raises(ValueError):
+        db_access.set_policy_attribute('https://example.com#nonexistent', 'TYPE', new_policy_type)
+
+    # Should reject changing attribute that is not permitted
+    with pytest.raises(ValueError):
+        db_access.set_policy_attribute(uri, 'NONEXISTENT', new_policy_type)
+
+
+def test_get_policy():
+    # Should raise an exception when the policy doesn't exist
+    uri = 'https://example.com#test'
+    with pytest.raises(ValueError):
+        db_access.get_policy(uri)
+
+    # Should get all the attributes of the policy
+    db_access.create_policy(uri)
+    result = db_access.get_policy(uri)
+    assert 'ID' in result
+    assert result['URI'] == 'https://example.com#test'
+
+
+def test_add_asset():
+    # Should raise an exception when the policy doesn't exist
+    asset_uri = 'https://example.com#asset'
+    policy_uri = 'https://example.com#policy'
+    with pytest.raises(ValueError):
+        db_access.add_asset(asset_uri, policy_uri)
+
+    # Should store a new asset entry in the database and assign an ID
+    db_access.create_policy(policy_uri)
+    new_asset_id = db_access.add_asset(asset_uri, policy_uri)
+    db_access.cursor.execute('SELECT URI FROM ASSET WHERE ID = {id}'.format(id=new_asset_id))
+    stored_uri = db_access.cursor.fetchone()[0]
+    assert stored_uri == asset_uri
+
+    # Should reject duplicate assets
+    with pytest.raises(ValueError):
+        db_access.add_asset(asset_uri, policy_uri)
+
+
+def test_asset_exists():
+    # Should return true if the asset exists
+    asset_uri = 'https://example.com#asset'
+    policy_uri = 'https://example.com#policy'
+    db_access.create_policy(policy_uri)
+    db_access.add_asset(asset_uri, policy_uri)
+    assert db_access.asset_exists(asset_uri)
+
+    # Should return false if the asset doesn't exist
+    nonexistent_uri = 'https://example.com#nonexistent'
+    assert not db_access.asset_exists(nonexistent_uri)
+
+
+def test_delete_policy():
+    # Should remove an existing policy
+    asset_uri = 'https://example.com#asset'
+    policy_uri = 'https://example.com#policy'
+    db_access.create_policy(policy_uri)
+    db_access.add_asset(asset_uri, policy_uri)
+    db_access.delete_policy(policy_uri)
+    assert not db_access.policy_exists(policy_uri)
+
+    # Should have also removed any assets using that policy
+    assert not db_access.asset_exists(asset_uri)
+
+
+def test_remove_asset():
+    # Should remove an existing asset
+    asset_uri = 'https://example.com#asset'
+    policy_uri = 'https://example.com#policy'
+    db_access.create_policy(policy_uri)
+    db_access.add_asset(asset_uri, policy_uri)
+    db_access.remove_asset(asset_uri)
+    assert not db_access.asset_exists(asset_uri)
+
+
+def test_create_rule():
+    # Should raise an exception when the rule type is not valid
+    with pytest.raises(ValueError):
+        db_access.create_rule('invalid_type')
+
+    # Should add a rule and return an id
+    rule_type = 'http://www.w3.org/ns/odrl/2/permission'
+    new_rule_id = db_access.create_rule(rule_type)
+    assert new_rule_id == 1
+    db_access.cursor.execute(
+        'SELECT COUNT(1) FROM RULE WHERE ID = {id} AND TYPE = "{type}"'.format(id=new_rule_id, type=rule_type)
+    )
+    rule_exists = db_access.cursor.fetchone()[0]
+    assert rule_exists
+
+
+def test_create_party():
+    # Should raise an exception when given an invalid uri
+    with pytest.raises(ValueError):
+        db_access.create_party('not a uri')
+
+    # Should store a new party entry in the database and assign an ID
+    new_party_uri = 'https://example.com#test'
+    new_party_id = db_access.create_party(new_party_uri)
+    assert new_party_id == 1
+    db_access.cursor.execute('SELECT URI FROM PARTY WHERE ID = {id}'.format(id=new_party_id))
+    stored_uri = db_access.cursor.fetchone()[0]
+    assert stored_uri == new_party_uri
+
+    # Should reject duplicate parties
+    with pytest.raises(ValueError):
+        db_access.create_party(new_party_uri)
+
+
+def test_party_exists():
+    # Should return true if the party exists
+    uri = 'https://example.com#test'
+    db_access.create_party(uri)
+    assert db_access.party_exists(uri)
+
+    # Should return false if the policy doesn't exist
+    nonexistent_uri = 'https://example.com#nonexistent'
+    assert not db_access.party_exists(nonexistent_uri)
+
+
+def test_delete_party():
+    # Should delete the entry for a party
+    party_uri = 'http://example.com#party'
+    db_access.create_party(party_uri)
+    db_access.delete_party(party_uri)
+    db_access.cursor.execute('SELECT COUNT(1) FROM PARTY WHERE URI = "{party_uri:s}"'.format(party_uri=party_uri))
+    action_entry_exists = db_access.cursor.fetchone()[0]
+    assert not action_entry_exists
+
+
+def test_rule_exists():
+    # Should return false if the policy doesn't exist
+    rule_id = 1
+    assert not db_access.rule_exists(rule_id)
+
+    # Should return true if the party exists
+    rule_type = 'http://www.w3.org/ns/odrl/2/permission'
+    rule_id = db_access.create_rule(rule_type)
+    assert db_access.rule_exists(rule_id)
+
+
+def test_add_action_to_rule():
+    # Should raise an exception if the rule doesn't exist
+    with pytest.raises(ValueError):
+        db_access.add_action_to_rule(1, None)
+
+    # Should raise an exception if the action doesn't exist
+    rule_type = 'http://www.w3.org/ns/odrl/2/permission'
+    rule_id = db_access.create_rule(rule_type)
+    action_uri = 'nonexistent'
+    with pytest.raises(ValueError):
+        db_access.add_action_to_rule(rule_id, action_uri)
+
+    # Should add an action to a rule
+    action_uri = 'http://www.w3.org/ns/odrl/2/distribute'
+    db_access.add_action_to_rule(rule_id, action_uri)
+    query = '''
+        SELECT COUNT(1) FROM RULE_HAS_ACTION R_A, ACTION A 
+        WHERE R_A.RULE_ID = {rule_id} 
+        AND R_A.ACTION_ID = A.ID
+        AND A.URI = "{action_uri}"
+    '''
+    db_access.cursor.execute(query.format(rule_id=rule_id, action_uri=action_uri))
+    action_entry_exists = db_access.cursor.fetchone()[0]
+    assert action_entry_exists
+
+
+def test_remove_action_from_rule():
+    # Should raise an exception if the action doesn't exist
+    rule_type = 'http://www.w3.org/ns/odrl/2/permission'
+    rule_id = db_access.create_rule(rule_type)
+    action_uri = 'nonexistent'
+    with pytest.raises(ValueError):
+        db_access.remove_action_from_rule(rule_id, action_uri)
+
+    # Should remove an action from a rule
+    action_uri = 'http://www.w3.org/ns/odrl/2/distribute'
+    db_access.add_action_to_rule(rule_id, action_uri)
+    db_access.remove_action_from_rule(rule_id, action_uri)
+    query = '''
+        SELECT COUNT(1) FROM RULE_HAS_ACTION R_A, ACTION A 
+        WHERE R_A.RULE_ID = {rule_id} 
+        AND R_A.ACTION_ID = A.ID
+        AND A.URI = "{action_uri}"
+    '''
+    db_access.cursor.execute(query.format(rule_id=rule_id, action_uri=action_uri))
+    action_entry_exists = db_access.cursor.fetchone()[0]
+    assert not action_entry_exists
+
+
+def test_get_party_id():
+    # Should raise an exception if the party doesn't exist
+    party_uri = 'http://example.com#party'
+    with pytest.raises(ValueError):
+        db_access.get_party_id(party_uri)
+
+    # Should retrieve an ID
+    db_access.create_party(party_uri)
+    int(db_access.get_party_id(party_uri))
+
+
+def test_add_assignor_to_rule():
+    # Should raise an exception if the rule doesn't exist
+    party_uri = 'http://example.com#party'
+    rule_id = 1
+    db_access.create_party(party_uri)
+    with pytest.raises(ValueError):
+        db_access.add_assignor_to_rule(party_uri, rule_id)
+
+    # Should raise an exception if the party doesn't exist
+    party_uri = 'nonexistent'
+    rule_type = 'http://www.w3.org/ns/odrl/2/permission'
+    rule_id = db_access.create_rule(rule_type)
+    with pytest.raises(ValueError):
+        db_access.add_assignor_to_rule(party_uri, rule_id)
+
+    # Should add an assignor to a rule
+    party_uri = 'http://example.com#party'
+    db_access.add_assignor_to_rule(party_uri, rule_id)
+    query = '''
+        SELECT COUNT(1) FROM RULE_HAS_ASSIGNOR R_A, PARTY P
+        WHERE R_A.RULE_ID = {rule_id}
+        AND R_A.PARTY_ID = P.ID
+        AND P.URI = "{party_uri}"
+    '''
+    db_access.cursor.execute(query.format(rule_id=rule_id, party_uri=party_uri))
+    assignor_exists = db_access.cursor.fetchone()[0]
+    assert assignor_exists
+
+
+def test_remove_assignor_from_rule():
+    # Should remove an action from a rule
+    party_uri = 'http://example.com#party'
+    db_access.create_party(party_uri)
+    rule_type = 'http://www.w3.org/ns/odrl/2/permission'
+    rule_id = db_access.create_rule(rule_type)
+    db_access.add_assignor_to_rule(party_uri, rule_id)
+    db_access.remove_assignor_from_rule(party_uri, rule_id)
+    query = '''
+        SELECT COUNT(1) FROM RULE_HAS_ASSIGNOR R_A, PARTY P
+        WHERE R_A.RULE_ID = {rule_id}
+        AND R_A.PARTY_ID = P.ID
+        AND P.URI = "{party_uri}"
+    '''
+    db_access.cursor.execute(query.format(rule_id=rule_id, party_uri=party_uri))
+    assignor_exists = db_access.cursor.fetchone()[0]
+    assert not assignor_exists
+
+
+def test_add_assignee_to_rule():
+    # Should raise an exception if the rule doesn't exist
+    party_uri = 'http://example.com#party'
+    rule_id = 1
+    db_access.create_party(party_uri)
+    with pytest.raises(ValueError):
+        db_access.add_assignee_to_rule(party_uri, rule_id)
+
+    # Should raise an exception if the party doesn't exist
+    party_uri = 'nonexistent'
+    rule_type = 'http://www.w3.org/ns/odrl/2/permission'
+    rule_id = db_access.create_rule(rule_type)
+    with pytest.raises(ValueError):
+        db_access.add_assignee_to_rule(party_uri, rule_id)
+
+    # Should add an assignee to a rule
+    party_uri = 'http://example.com#party'
+    db_access.add_assignee_to_rule(party_uri, rule_id)
+    query = '''
+        SELECT COUNT(1) FROM RULE_HAS_ASSIGNEE R_A, PARTY P
+        WHERE R_A.RULE_ID = {rule_id}
+        AND R_A.PARTY_ID = P.ID
+        AND P.URI = "{party_uri}"
+    '''
+    db_access.cursor.execute(query.format(rule_id=rule_id, party_uri=party_uri))
+    assignee_exists = db_access.cursor.fetchone()[0]
+    assert assignee_exists
+
+
+def test_remove_assignee_from_rule():
+    # Should remove an action from a rule
+    party_uri = 'http://example.com#party'
+    db_access.create_party(party_uri)
+    rule_type = 'http://www.w3.org/ns/odrl/2/permission'
+    rule_id = db_access.create_rule(rule_type)
+    db_access.add_assignee_to_rule(party_uri, rule_id)
+    db_access.remove_assignee_from_rule(party_uri, rule_id)
+    query = '''
+        SELECT COUNT(1) FROM RULE_HAS_ASSIGNEE R_A, PARTY P
+        WHERE R_A.RULE_ID = {rule_id}
+        AND R_A.PARTY_ID = P.ID
+        AND P.URI = "{party_uri}"
+    '''
+    db_access.cursor.execute(query.format(rule_id=rule_id, party_uri=party_uri))
+    assignee_exists = db_access.cursor.fetchone()[0]
+    assert not assignee_exists
